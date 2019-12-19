23ffb2619f0d2625279701485c8c735bee385de5
german Rovati <germanrovati@Germans-MacBook-Pro-3.local>
2019-12-18 19:20:35 -0500

    changes to webpack location and comments

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+// 
 import { RootState } from '../reducers'
 import { Button } from '../components/common/Button'
 import { incrementActionCreator } from '../actions';
 // import {} from '../actions'
 // import { loggedInSelector } from '../reducers/login'
 
+// sets type for props
 interface AppProps {
   incrementActionCreator: (...arg: any[]) => any
   // loggedIn: boolean
@@ -28,7 +30,7 @@ class App extends Component<AppProps> {
   }
 }
 
-
+// gives the app component access to state and actions from the store
 export default connect(
   (state: RootState) => ({
     increment: state.increment
