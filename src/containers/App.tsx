4c70267327a99e242d75b6dac1f0d7c017f756a4
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2019-12-19 00:23:10 -0500

    imported new containers; basic inline styling

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,6 +2,11 @@ import React, { FunctionComponent } from 'react'
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { RootState } from '../reducers'
+import HeaderContainer, { Header } from './Header'
+import BodyContainer from './Body'
+import FooterContainer from './Footer'
+import NavbarContainer from './Navbar'
+
 import { Button } from '../components/common/Button'
 import { incrementActionCreator } from '../actions';
 import { countSelector } from '../reducers/test';
@@ -19,10 +24,23 @@ export const App: FunctionComponent<AppProps> = props => {
       incrementAction
     } = props
     return (
-      <>
-        <Button text='enter' onClick={ () => {incrementAction(1)}} >
-        </Button>
-      </>
+      <div>
+        <div style = {{display: "flex", flexDirection: "row", height: "100%", alignItems: "stretch"}}>
+          <div style = {{border: "solid 1px black"}}>
+            <NavbarContainer></NavbarContainer>
+          </div>
+          
+          <div style = {{border: "solid 1px red", display: "flex", flexDirection: "column", width: "100%"}}>
+            <HeaderContainer ></HeaderContainer>
+            <BodyContainer>
+            </BodyContainer>
+            <FooterContainer></FooterContainer>
+          </div>
+          <Button text='enter' onClick={ () => {incrementAction(1)}} >
+          </Button>
+        </div>
+
+      </div>
     )
   }
 }
