5060bcecc7a80a530d5d7780b1a3bd79ac905ccb
James <me@jameshighsmith.com>
2019-12-21 14:18:53 -0500

    updated to take multiple fields form the proto

--- grpc_server/grpc_server.js
+++ grpc_server/grpc_server.js
@@ -1,8 +1,9 @@
-// this is our test grpc server:
-let PROTO_PATH = __dirname + '/../protos/helloworld.proto';
-const port = '0.0.0.0:50051';
 let grpc = require('grpc');
 let protoLoader = require('@grpc/proto-loader');
+
+// this is our test grpc server:
+let PROTO_PATH = __dirname + '/../protos/helloworld.proto';
+// const port = '0.0.0.0:50051';
 
 let packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -22,10 +23,21 @@ let hello_proto = grpc.loadPackageDefinition(packageDefinition)[packageName];
 /**
  * Implements the SayHello RPC method.
  */
+let port;
+
 function sayHello(call, callback) {
   console.log('------------')
   console.log(`Hi! I'm the gRPC server`)
-  callback(null, {message: 'Hello ' + call.request.name});
+  // items coning in from the express server:
+  console.log(call.request)
+  port = call.request.port;
+  let packageName = call.request.packageName;
+  let service = call.request.service;
+  let message = call.request.message;
+  let protoObject = call.request.protoObject;
+
+
+  callback(null, {message: 'Hello ' + packageName + `! This was a call to port ${port} requesting the ${service} in ${protoObject} with the message of: ${message}`});
   // console.log('after grpc server')
 }
 /**
@@ -36,7 +48,8 @@ function main() {
   let server = new grpc.Server();
   server.addService(hello_proto.YodelayAPI.service, 
     { sayHello });
-  server.bind(port, grpc.ServerCredentials.createInsecure());
+    console.log(port);
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
 }
 
