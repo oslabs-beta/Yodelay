91c62ef1962024620bd9525dd063ebb8d0e9f931
James <me@jameshighsmith.com>
2019-12-21 12:23:58 -0500

    helper working with postman POST

--- server_client/helper_request_func.js
+++ server_client/helper_request_func.js
@@ -1,8 +1,9 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const grpc_promise = require('grpc-promise');
-// grab the proto path:
+// when the .proto file is local we use this method to pass it to our grpcRequest helper function
 const PROTO_PATH = __dirname + '/../protos/helloworld.proto';
+// grab the proto path:
 const fs = require('fs');
 
 const CONFIG_OBJECT = {
@@ -12,46 +13,64 @@ const CONFIG_OBJECT = {
   defaults: true,
   oneofs: true
 }
+
+// 
+// Local File:
+// 
 // synchronously compiles and loads the .proto file into a definition
 // this will be moved to the front end:
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, CONFIG_OBJECT);
-
-// let's see if we can turn it into a json object to be passed to the backend:
-const stringDefinition = JSON.stringify(packageDefinition);
-
-//  Let's check to see if we stringifyed it!
-// console.log("stringify: ", stringDefinition);
-
-// this is where the frontend passes to the backend:
-console.log('______Passed in from the Front End_______');
-
-
-// now let's write a file to the proto file with our passed in stringified proto file: 
-fs.writeFile("./protos/output.proto", stringDefinition, 'utf8', function (err) {
-  if (err) {
-    console.log("An error occurred while writing JSON Object to File.");
-    return console.log(err);
-  }
-  console.log("JSON file has been saved.");
-});
-
-// this is the file we are going to write the proto to:
-const inputProto = require('../protos/output.proto');
-
-console.log(inputProto)
-// Now let's parse it back :
-const JSONDefinition = JSON.parse(inputProto);
-console.log('JSONDefinition: ', JSONDefinition);
-
-
-
 // * generates a descriptor Object from the loaded API definition
-const descriptor = grpc.loadPackageDefinition(JSONDefinition).helloworld;
+const descriptor = grpc.loadPackageDefinition(packageDefinition).helloworld;
 // console.log(descriptor);
 
 
+// 
+// JSON Object: 
+// 
+// when the .proto file is passed as a JSON object we use this method to pass it to our grpcRequest helper function
+// FRONT END:
+// const stringDefinition = JSON.stringify(packageDefinition);
+//  Let's check to see if we stringifyed it!
+// console.log("stringify: ", stringDefinition);
+// this is where the frontend passes to the backend:
+// console.log('______Passed in from the Front End_______');
+// 
+// BACK END:
+// 
+// Write the JSON object locally: 
+// fs.writeFile("./protos/output.proto", stringDefinition, 'utf8', function (err) {
+//   if (err) {
+//     console.log("An error occurred while writing JSON Object to File.");
+//     return console.log(err);
+//   }
+//   console.log("JSON file has been saved.");
+// });
+// File location:
+// this is the file we are going to write the proto to:
+// const inputProto = require('../protos/output.proto');
+// console.log(inputProto)
+// Parse:
+// const JSONDefinition = JSON.parse(inputProto);
+// console.log('JSONDefinition: ', JSONDefinition);
+
+// 
+// STRING:
+// 
+// when a string is passed to the back end we will then use this method:
+// 
+// first we 
+// 
+
 
 function grpcRequest(input) {
+// 
+// STRING:
+// 
+// when a string is passed to the back end as a string we will then use this method:
+// it's passed to express on the req.body which we pass in as the input 
+  console.log("grpcRequest input: ", input)
+
   let port = input.port
   // declare the package.
   const package = new descriptor.YodelayAPI(port, grpc.credentials.createInsecure());
