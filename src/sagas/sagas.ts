09b9f053b9ebb0888042f7e8091776dc1341f306
Cedric Theofanous <cedrictheo@gmail.com>
2019-12-22 13:39:32 -0500

    update saga body will not send in fetch

--- src/sagas/sagas.ts
+++ src/sagas/sagas.ts
@@ -1,23 +1,27 @@
-import { call, put, takeLatest} from 'redux-saga/effects'
-import moment from 'moment'
-import {SEND_PROTO, sendProtoAction} from '../actions/index'
+import { call, put, takeLatest } from 'redux-saga/effects';
+// import moment from 'moment'
+import { UPLOAD_PROTO, uploadProtoAction } from '../actions/index';
+import { json } from 'body-parser';
 
-function* sendProto({payload: string | ArrayBuffer}: sendProtoAction){
+// chaining this saga middleware after uploadProto to post the file string to the server
+function* sendProto({ payload: type, payload }: uploadProtoAction) {
+  // yield call(fetch, '/scores', { method: 'GET', body: { score } })
   try {
-    const protoDescription = yield call(, '/upload')
-    fetch('https://localhost:3000', {
+    yield call(fetch, 'http://localhost:4000/upload', {
       method: 'POST',
-      body: JSON.stringify()
-      
-    })
-
-  } catch ({error, status}) {
-    console.log(error, status)
+      body: payload,
+      // headers: {
+      //   'Content-Type': 'text/plain'
+      // }
+    });
+  } catch ({ error, status }) {
+    console.log('error in upload saga', error, status);
   }
 }
 
 function* saga() {
-
+  console.log('saga');
+  yield takeLatest(UPLOAD_PROTO, sendProto);
 }
 
-export default saga
+export default saga;
