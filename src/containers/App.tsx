bc9a2bfe965e28914ec37338b04a51a9a589f96d
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2019-12-23 18:05:27 -0500

    wrapped all rendered containers in Router tag

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react'
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { RootState } from '../reducers'
 import HeaderContainer, { Header } from './Header'
 import BodyContainer from './Body'
 import FooterContainer from './Footer'
 import NavbarContainer from './Navbar'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import { Button } from '../components/common/Button'
 import { incrementActionCreator } from '../actions';
@@ -24,22 +24,24 @@ export const App: FunctionComponent<AppProps> = props => {
       incrementAction,
     } = props
     return (
-      <div>
-        <div style = {{display: "flex", flexDirection: "row", height: "100%", alignItems: "stretch"}}>
-          <div style = {{border: "solid 1px black"}}>
-            <NavbarContainer></NavbarContainer>
+      //Wrap everything in Router so that nested containers/components have access to router
+      <Router>
+        <div>
+          <div style = {{display: "flex", flexDirection: "row", height: "100%", alignItems: "stretch"}}>
+            <div style = {{border: "solid 1px black"}}>
+              <NavbarContainer></NavbarContainer>
+            </div>
+            
+            <div style = {{border: "solid 1px red", display: "flex", flexDirection: "column", width: "100%"}}>
+              <HeaderContainer ></HeaderContainer>
+              <BodyContainer></BodyContainer>
+              <FooterContainer></FooterContainer>
+            </div>
+            <Button text='enter' onClick={ () => {incrementAction(1)}} >
+            </Button>
           </div>
-          
-          <div style = {{border: "solid 1px red", display: "flex", flexDirection: "column", width: "100%"}}>
-            <HeaderContainer ></HeaderContainer>
-            <BodyContainer></BodyContainer>
-            <FooterContainer></FooterContainer>
-          </div>
-          <Button text='enter' onClick={ () => {incrementAction(1)}} >
-          </Button>
         </div>
-
-      </div>
+      </Router>
     )
   }
 }
