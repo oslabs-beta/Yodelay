6e03e9178c57f62a516bea428a2e6500b6575534
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2019-12-23 18:10:02 -0500

    added placeholder

---
+++ src/components/Settings.tsx
@@ -0,0 +1,38 @@
+import React, {FunctionComponent } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// sets type for props
+interface SettingsProps {
+
+  }
+  
+  export const Settings: FunctionComponent<SettingsProps> = props => {
+    {
+    //   const {
+    //     incrementAction
+    //   } = props
+      return (
+        <div style = {{border: "solid 1px green", flexGrow: 2}}>
+        Settings
+        </div>
+      )
+    }
+  }
+  
+
+export default Settings
+
+  // gives the app component access to state and actions from the store
+//   export default connect(
+  
+//     //using selector
+//     (state: RootState) => ({
+//         test: countSelector(state)
+//       })
+//       ,
+    
+//     {
+//       incrementAction: incrementActionCreator,
+//     }
+//   )(App)
