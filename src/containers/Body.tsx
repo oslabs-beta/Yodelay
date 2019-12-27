b8fa4ac34156d30305dc46f76304c79411d1e846
Cedric Theofanous <cedrictheo@gmail.com>
2019-12-27 11:40:36 -0500

    add bracket (refactor)

--- src/containers/Body.tsx
+++ src/containers/Body.tsx
@@ -1,55 +1,45 @@
-import React, {FunctionComponent, useRef } from 'react'
-import { connect } from 'react-redux'
-import TestProto from '../components/TestProto'
-import Settings from '../components/Settings'
-import { Route } from 'react-router'
+import React, { FunctionComponent, useRef } from 'react';
+import { connect } from 'react-redux';
+import TestProto from '../components/TestProto';
+import Settings from '../components/Settings';
+import { Route } from 'react-router';
 
 // sets type for props
-interface BodyProps {
+interface BodyProps {}
 
-  }
+export const Body: FunctionComponent<BodyProps> = props => {
+  {
+    const serverInputRef = useRef<HTMLInputElement>();
 
-
-
-  export const Body: FunctionComponent<BodyProps> = props => {
-    {
-
-      const serverInputRef = useRef<HTMLInputElement>();
-
-      // handle serverInput change
-      const handleChange = (e: any) => {
-        serverInputRef.current.value = e.target.value;
-        console.log(
-          'serverInputRef.current.value:',
-          serverInputRef.current.value
-        );
-      };
+    // handle serverInput change
+    const handleChange = (e: any) => {
+      serverInputRef.current.value = e.target.value;
+      console.log(
+        'serverInputRef.current.value:',
+        serverInputRef.current.value
+      );
+    };
 
     //   const {
     //     incrementAction
     //   } = props
-      return (
-        <div style = {{border: "solid 1px green", flexGrow: 2}}>
-                  <input
+    return (
+      <div style={{ border: 'solid 1px green', flexGrow: 2 }}>
+        <input
           ref={serverInputRef}
           id="serverInput"
           placeholder="enter server ip address"
           onChange={handleChange}
         ></input>
         Body
-          <Route exact path = "/"> 
-            <TestProto>
-              Test Proto
-            </TestProto>
-          </Route>
-          <Route path = "/settings"> 
-            <Settings>
-              Settings
-            </Settings>
-          </Route>
-        </div>
-      )
-    }
+        <Route exact path="/">
+          <TestProto>Test Proto</TestProto>
+        </Route>
+        <Route path="/settings">
+          <Settings>Settings</Settings>
+        </Route>
+      </div>
+    );
   }
 };
 
