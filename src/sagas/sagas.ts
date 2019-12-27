8631140a7c9eeb3817aed9c2ff81fa3143eaf97f
Cedric Theofanous <cedrictheo@gmail.com>
2019-12-27 00:01:46 -0500

    grab response from uploadProto fetch

--- src/sagas/sagas.ts
+++ src/sagas/sagas.ts
@@ -15,16 +15,26 @@ function* sendProto({ payload: type, payload }: uploadProtoAction) {
         'Content-Type': 'text/plain'
       },
       body: jsonProtoFile
-    });
-  // todo: .then response here! send back with service and message fields filled out:
-  // todo  {
-  // todo     "port": "0.0.0.0:50051",
-  // todo     "packageName": "WorLd",
-  // todo     "service": "YodelayAPI",
-  // todo     "message": "SayHello",
-  // todo     "protoObject": "syntax = 'proto3'; package helloaworld; service YodelayAPI { rpc SayHello (HelloRequest) returns (HelloReply) {} }message HelloRequest { string port = 1; string packageName = 2; string service = 3; string message = 4; string protoObject = 5; } message HelloReply { string message = 1; }"
-  // todo }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // console.log('here is the protoObject returned when a protoFile is uploaded:', data);
+        put({
+          type: 'PROTO_UPLOAD_SUCCEEDED',
+          protoObject: data
+        });
+      });
+
+    // todo: .then response here! send back with service and message fields filled out:
+    // todo  {
+    // todo     "port": "0.0.0.0:50051",
+    // todo     "packageName": "WorLd",
+    // todo     "service": "YodelayAPI",
+    // todo     "message": "SayHello",
+    // todo     "protoObject": "syntax = 'proto3'; package helloaworld; service YodelayAPI { rpc SayHello (HelloRequest) returns (HelloReply) {} }message HelloRequest { string port = 1; string packageName = 2; string service = 3; string message = 4; string protoObject = 5; } message HelloReply { string message = 1; }"
+    // todo }
   } catch ({ error, status }) {
+    yield put({ type: 'PROTO_UPLOAD_FAILED' });
     console.log('error in upload saga', error, status);
   }
 }
