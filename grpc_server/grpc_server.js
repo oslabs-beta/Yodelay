7b4fab83d1b414d49c96011b3c718c003ae87368
James <me@jameshighsmith.com>
2020-01-01 16:04:30 -0500

    updated callback message

--- grpc_server/grpc_server.js
+++ grpc_server/grpc_server.js
@@ -23,25 +23,29 @@ let hello_proto = grpc.loadPackageDefinition(packageDefinition)[packageName];
 /**
  * Implements the SayHello RPC method.
  */
-let port;
+let url;
 
 function sayHello(call, callback) {
   console.log('------rpc function call------')
   console.log(`Hi! I'm the gRPC server`)
   // items coning in from the express server:
-  // console.log('gRPC incoming call request: ', call.request)
-  port = call.request.port;
-  console.log('inside sayHello port: ', port)
-  let packageName = call.request.packageName;
-  let service = call.request.service;
-  let message = call.request.message;
-  let protoObject = call.request.protoObject;
+  console.log('gRPC incoming call request: ', call.request)
+  url = call.request.url;
+  let package = call.request.package;
+  let serviceInput = call.request.serviceInput;
+  let requestInput = call.request.requestInput;
+  let messageInput = call.request.messageInput;
+  let protoFile = call.request.protoFile;
+  let protoDescriptor = call.request.protoDescriptor;
   
   
-  callback(null, {message: 'Hello ' + packageName + `! This was a call to port ${port} requesting the ${service} in ${protoObject} with the message of: ${message}`});
+  // callback(null, {message: 'Hello ' + package + `! This was a call to port ${url} requesting the ${serviceInput} in ${protoFile} with the message of: ${messageInput}`});
+  callback(null, {
+    message: `This was a call to ${url} using the ${packageName} package with the ${serviceInput} that contains the ${requestInput} method with the message of: ${messageInput}. We were able to get this from parsing ${protoFile} with the built in ${protoDescriptor}.`
+  });
   // console.log('after grpc server')
 }
-console.log('port after: ', port)
+
 /**
  * Starts an RPC server that receives requests for the Greeter service at the
  * sample server port
@@ -50,11 +54,11 @@ function main() {
   let server = new grpc.Server();
   server.addService(hello_proto.YodelayAPI.service, 
     { sayHello });
-    console.log('grpc main port: ', port);
+    console.log('grpc main url: ', url);
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
 }
 
 main();
 
-console.log(`gRPC💥 'n on port: 0.0.0.0:50051 (${port})`)
+console.log(`gRPC💥 'n on port: 0.0.0.0:50051 (${url})`)
