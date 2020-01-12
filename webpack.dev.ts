ec483adee25f43ab20383e44865985fa1db284b1
James <me@jameshighsmith.com>
2020-01-12 18:10:08 -0500

    updated for images

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -27,12 +27,21 @@ const config = (env: any): webpack.Configuration => {
           loader: 'awesome-typescript-loader'
         },
         {
-          test: /\.(sc|c)ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader']
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
         },
         {
           test: /\.(png|svg|jpg|gif)$/,
-          use: { loader: 'file-loader'}
+          use: [
+            'file-loader',
+          ],
         },
       ]
     },
