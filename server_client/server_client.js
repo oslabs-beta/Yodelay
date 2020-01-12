e43f5c64aaada7ca6de5226feef01edff08a726b
James <me@jameshighsmith.com>
2020-01-11 20:21:39 -0500

    improve endpoints

--- server_client/server_client.js
+++ server_client/server_client.js
@@ -2,22 +2,19 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const { grpcRequest, parseProto } = require('./helper_request_func');
-
 const app = express();
 // changed to port 4000 because react hot module runs on 3000
 const port = 4000;
-
 // this line hackily solves the CORS errors when sending post requests to /upload
 // refactor eventually
 app.use(cors());
-
 // Parsing!
 //  parse to text (bodyParser.json breaks upload fetch request)
 app.use(bodyParser.text());
 // parse cookies
 app.use(cookieParser());
-
 // uncomment this flow test to see all incoming requests printed -- of great help while debugging
 // app.use((req, res, next) => {
 //   console.log('*************************************************************');
@@ -34,23 +31,21 @@ app.use(cookieParser());
 //   console.log('*************************************************************');
 //   next();
 // });
-
 // Root:
-app.get('/', (req, res) => res.send('🍻  Yodelay World  🍻'));
-
+// app.get('/', (req, res) => res.send(':beers:  Yodelay World  :beers:'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../build/index.html')));
+app.get('/bundle.js', (req, res) => res.sendFile(path.resolve(__dirname, '../build/bundle.js')));
 // * UPLOAD:
 // when we hit the /upload endpoint we take in the request body and pass it as an argument to the helper request function:
 // Input req.body
-// output: {protoFile: “the text of the photo file”, services: [{}, {}, {}], protoDescription: {}}
+// output: {protoFile: "the text of the photo file", services: [{}, {}, {}], protoDescription: {}}
 app.post('/upload', async (req, res) => {
   // was able to send the file so that a simple JSON.parse takes care of it
   console.log(
     'HERE IS THE PARSED PROTOFILE STRING SENT TO /UPLOAD ENPOINT:',
     JSON.parse(req.body)
   );
-
   const parsedReqBody = JSON.parse(req.body)
-
   // console.log('---req.body:', req.body, '---/upload req---');
   // to our grpc request function
   // console.log('/upload req.body: ', req.body)
@@ -60,8 +55,6 @@ app.post('/upload', async (req, res) => {
   // this is goiing to be the protoFile, services we pull and the protoDescription:
   res.json(output);
 });
-
-
 // * SERVICE:
 // * Start GRPC Server Call:
 // front end sends back the request when they hit the /service endpoint.
@@ -75,10 +68,8 @@ app.post('/service', async (req, res) => {
   // then send response with the output that's been jsonified.
   res.json(output);
 });
-
 // *
 // * End GRPC Server Call
-
 //cedric's upload path (likely to be deleted)
 // app.post('/upload', (req, res) => {
 //   console.log(
@@ -87,12 +78,10 @@ app.post('/service', async (req, res) => {
 //   );
 //   res.json('protoFile uploaded succesfully man');
 // });
-
 // Unknown Route:
 app.use((req, res) => {
   res.status(404).send('Page Not Found');
 });
-
 // Global error handling:
 app.use(function(err, req, res, next) {
   const defaultError = {
@@ -104,7 +93,6 @@ app.use(function(err, req, res, next) {
   console.log(newErrObj);
   res.status(newErrObj.status).json(newErrObj.message);
 });
-
 app.listen(port, () =>
-  console.log(`  👽  invasion happening on port: ${port} `)
+  console.log(`  :alien:  invasion happening on port: ${port} `)
 );
