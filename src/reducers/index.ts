d0c87a78e4349d51c425e9fbfc781808ff54e9e0
Tammy Tan <tammytan95@Tammys-MacBook-Air.local>
2020-01-14 15:43:15 -0500

    added changeTheme state mgmt

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,21 +1,23 @@
 import { combineReducers } from "redux";
 // needs clarifying -- how does it recognizes increment and why does it need an alias
-import {test, testState} from './test'
-import {uploadProto, initialProtoStateType} from './uploadProto'
-import {updateMenu, initialMenuStateType} from './updateMenu'
-
+import { test, testState } from "./test";
+import { uploadProto, initialProtoStateType } from "./uploadProto";
+import { updateMenu, initialMenuStateType } from "./updateMenu";
+import { changeTheme, initialThemeStateType } from "./changeTheme";
 
 export interface RootState {
-  test: testState
-  uploadProto: initialProtoStateType
-  updateMenu: initialMenuStateType
+  test: testState;
+  uploadProto: initialProtoStateType;
+  updateMenu: initialMenuStateType;
+  changeTheme: initialThemeStateType;
 }
 
 // Turns an object whose values are different reducing functions into a single reducing function you can pass to createStore
-const rootReducer = combineReducers ({
+const rootReducer = combineReducers({
   test,
-  uploadProto, 
-  updateMenu
-})
+  uploadProto,
+  updateMenu,
+  changeTheme
+});
 
 export default rootReducer;
