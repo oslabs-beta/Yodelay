03f777ba77f6c0dc059b49150a11fedda8f81c35
James <me@jameshighsmith.com>
2020-01-14 18:01:29 -0500

    update

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -57,7 +57,7 @@ const config = (env: any): webpack.Configuration => {
       new webpack.DefinePlugin({
         'env.API_PORT': API_PORT
           ? JSON.stringify(API_PORT)
-          : JSON.stringify('8000'),
+          : JSON.stringify('8080'),
         'env.API_HOST': API_HOST
           ? JSON.stringify(API_HOST)
           : JSON.stringify('www.yodelay.io'),
